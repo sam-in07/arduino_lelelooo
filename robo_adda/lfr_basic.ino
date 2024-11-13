b9c195a0b954a9e868e92e667e433bac015a71ea
Samin Ahmed Chowdhury <85004796+sam-in07@users.noreply.github.com>
2024-11-13 23:00:33 +0600

    Create lfr_basic.ino

---
+++ robo_adda/lfr_basic.ino
@@ -0,0 +1,14 @@
+//digital read and digital write
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13,OUTPUT) ;
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(13,LOW) ;
+delay(1000);
+digitalWrite(13,LOW) ;
+delay(1000);
+}
