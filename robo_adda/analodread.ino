692861ee78d761f9dacff042b87ce4860718dc98
Samin Ahmed Chowdhury <85004796+sam-in07@users.noreply.github.com>
2024-11-14 17:20:06 +0600

    Create analodread.ino

---
+++ robo_adda/analodread.ino
@@ -0,0 +1,20 @@
+int x;
+
+void setup() {
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  x = analogRead(A0);
+  Serial.println(x);
+  if (x > 500) {
+    digitalWrite(13, HIGH);
+  } else {
+    digitalWrite(13, LOW);
+  }
+}
+/*
+
+This code reads an analog value from pin A0, 
+prints it to the serial monitor, and turns an LED on pin 13 on or off based on whether the value is greater than 500. */
